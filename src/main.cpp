165321f1245aa3d6e8a2a366407f86b275318d88
Krona-org <danil.marushkin@yandex.ru>
2026-06-23 01:30:23 +0300

    Code refactor

--- src/main.cpp
+++ src/main.cpp
@@ -18,15 +18,13 @@ static void fillArray(int* array, const int sizeArray, const int min, const int 
 
 static void shellSort(int* array, const int sizeArray) {
 
-    for(int gap = sizeArray / 2; gap > 0; gap /= 2)
-    {
-        for(int i = gap; i < sizeArray; i +=1)
-        {
+    for(int gap = sizeArray / 2; gap > 0; gap /= 2) {
+        for(int i = gap; i < sizeArray; i++) {
             int j, temp = array[i];
 
             for(j = i; j >= gap && array[j - gap] > temp; j -= gap)
                 array[j] = array[j - gap];
-
+            
             array[j] = temp;
         }
     }
@@ -35,12 +33,18 @@ static void shellSort(int* array, const int sizeArray) {
 static void readFromFile(const char* path, int*& array, int& size) {
 
     std::ifstream file(path);
-    if (!file.is_open()) 
-        std::cerr << "Not Open File\n";
+    if (!file.is_open()) {
+        std::cerr << "Invalid path file";
+        return;
+    }
 
     int num, count = 0;
     while(file >> num) count++;
 
+    if(!file.eof()) {        
+        std::cerr << "Failed to write file";
+        return;
+    }
     file.clear();
     file.seekg(0);
 
@@ -61,9 +65,7 @@ static void writeToFile(const char* path, const int* array, const int& size) {
         std::cerr << "Not Open File\n";
 
     for(int i = 0; i < size; i++)
-    {
         file << array[i] << " ";
-    }
 
     file.close();
 }
@@ -76,9 +78,9 @@ int main() {
     int sizeArray = 30;
     int* array = new int[sizeArray];
 
-    fillArray(array, sizeArray, 0, 30);
+    //fillArray(array, sizeArray, 0, 30);
 
-    writeToFile(input, array, sizeArray);
+    //writeToFile(input, array, sizeArray);
     readFromFile(input, array, sizeArray);
     std::cout << "Read from file input: ";
     printArr(array, sizeArray);
